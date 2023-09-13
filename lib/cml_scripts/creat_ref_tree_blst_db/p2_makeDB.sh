0aa51ab85ccef5edc10945dbc2fbd90c052b23f3
DaisyBrumit <dfrybrum@uncc.edu>
2023-09-13 10:04:35 -0400

    remove any duplicates in treeFasta.fasta

--- lib/cml_scripts/creat_ref_tree_blst_db/p2_makeDB.sh
+++ lib/cml_scripts/creat_ref_tree_blst_db/p2_makeDB.sh
@@ -4,6 +4,10 @@
 module load blast
 
 cd ~/git/lognorm_vs_CODA/lib/ref_tree_objs
+
+# remove any possible duplicate values in treeFasta.fasta
+awk 'BEGIN {i = 1;} { if ($1 ~ /^>/) { tmp = h[i]; h[i] = $1; } else if (!a[$1]) { s[i] = $1; a[$1] = "1"; i++; } else { h[i] = tmp; } } END { for (j = 1; j < i; j++) { print h[j]; print s[j]; } }' treeFasta.fasta > treeFastaout.fasta
+
 # -parse_seqids option is required to keep the original sequence identifiers. Otherwise makeblastdb will generate its own identifiers.
 makeblastdb -in treeFasta.fasta -out db/tree -parse_seqids -dbtype nucl
 
